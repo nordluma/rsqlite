d215d3d846610c81ec3452fa4f539e4b068904ce
nordluma <maxnordlund.95@hotmail.com>
2024-11-22 00:10:07 +0200

    feat(page): define types to represent a table btree-leaf

--- src/page.rs
+++ src/page.rs
@@ -2,3 +2,36 @@
 pub struct DbHeader {
     pub page_size: u32,
 }
+
+#[derive(Debug, Clone)]
+pub enum Page {
+    TableLeaf(TableLeafPage),
+}
+
+#[derive(Debug, Clone)]
+pub struct TableLeafPage {
+    pub header: PageHeader,
+    pub cell_pointers: Vec<u16>,
+    pub cells: Vec<TableLeafCell>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PageHeader {
+    pub page_type: PageType,
+    pub first_freeblock: u16,
+    pub cell_count: u16,
+    pub cell_content_offset: u32,
+    pub fragmented_bytes_count: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PageType {
+    TableLeaf,
+}
+
+#[derive(Debug, Clone)]
+pub struct TableLeafCell {
+    pub size: i64,
+    pub row_id: i64,
+    pub payload: Vec<u8>,
+}
