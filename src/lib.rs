641ea55eaebc83111634f699a14fde68a995fec1
nordluma <maxnordlund.95@hotmail.com>
2024-11-23 00:33:08 +0200

    feat(value): define type to hold values

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod cursor;
 pub mod page;
 pub mod pager;
+pub mod value;
---
+++ src/value.rs
@@ -0,0 +1,19 @@
+use std::borrow::Cow;
+
+#[derive(Debug, Clone)]
+pub enum Value<'p> {
+    Null,
+    String(Cow<'p, str>),
+    Blob(Cow<'p, [u8]>),
+    Int(i64),
+    Float(f64),
+}
+
+impl<'p> Value<'p> {
+    pub fn as_str(&self) -> Option<&str> {
+        match self {
+            Value::String(s) => Some(s.as_ref()),
+            _ => None,
+        }
+    }
+}
