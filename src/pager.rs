3e0af0a73d4f61914603d53e7596f58f2afa61c2
nordluma <maxnordlund.95@hotmail.com>
2024-11-22 18:52:17 +0200

    feat(pager): define functions to parse table btree-leaf pages

--- src/pager.rs
+++ src/pager.rs
@@ -1,10 +1,22 @@
+use std::usize;
+
 use crate::page;
 
+// header consts
 pub const HEADER_SIZE: usize = 100;
 const HEADER_PREFIX: &[u8] = b"SQLITE format 3\0";
 const HEADER_PAGE_SIZE_OFFSET: usize = 16;
 
 const PAGE_MAX_SIZE: u32 = 65536;
+const PAGE_LEAF_HEADER_SIZE: usize = 8;
+
+const PAGE_LEAF_TABLE_ID: u8 = 13;
+
+const PAGE_LEAF_HEADER: usize = 8;
+const PAGE_FIRST_FREEBLOCK_OFFSET: usize = 1;
+const PAGE_CELL_COUNT_OFFSET: usize = 3;
+const PAGE_CELL_CONTENT_OFFSET: usize = 5;
+const PAGE_FRAGMENTED_BYTES_COUNT_OFFSET: usize = 7;
 
 pub fn parse_header(buffer: &[u8]) -> Result<page::DbHeader, anyhow::Error> {
     if !buffer.starts_with(HEADER_PREFIX) {
@@ -20,6 +32,118 @@ pub fn parse_header(buffer: &[u8]) -> Result<page::DbHeader, anyhow::Error> {
     };
 
     Ok(page::DbHeader { page_size })
+}
+
+fn parse_page(buffer: &[u8], page_num: usize) -> Result<page::Page, anyhow::Error> {
+    // skip the database header if this is the first page we are parsing
+    let ptr_offset = if page_num == 1 { HEADER_SIZE as u16 } else { 0 };
+
+    match buffer[0] {
+        PAGE_LEAF_TABLE_ID => parse_table_leaf_page(buffer, ptr_offset),
+        _ => Err(anyhow::anyhow!("unknown page type: {}", buffer[0])),
+    }
+}
+
+fn parse_table_leaf_page(buffer: &[u8], ptr_offset: u16) -> Result<page::Page, anyhow::Error> {
+    let header = parse_page_header(buffer)?;
+
+    let content_buffer = &buffer[PAGE_LEAF_HEADER_SIZE..];
+    let cell_pointers = parse_cell_pointers(content_buffer, header.cell_count as usize, ptr_offset);
+
+    let cells = cell_pointers
+        .iter()
+        .map(|&ptr| parse_table_leaf_cell(&buffer[ptr as usize..]))
+        .collect::<Result<Vec<page::TableLeafCell>, anyhow::Error>>()?;
+
+    Ok(page::Page::TableLeaf(page::TableLeafPage {
+        header,
+        cell_pointers,
+        cells,
+    }))
+}
+
+fn parse_page_header(buffer: &[u8]) -> Result<page::PageHeader, anyhow::Error> {
+    let page_type = match buffer[0] {
+        0x0d => page::PageType::TableLeaf,
+        _ => anyhow::bail!("unknown page type: {}", buffer[0]),
+    };
+
+    let first_freeblock = read_be_word_at(buffer, PAGE_FIRST_FREEBLOCK_OFFSET);
+    let cell_count = read_be_word_at(buffer, PAGE_CELL_COUNT_OFFSET);
+    let cell_content_offset = match read_be_word_at(buffer, PAGE_CELL_CONTENT_OFFSET) {
+        0 => PAGE_MAX_SIZE,
+        n => n as u32,
+    };
+    let fragmented_bytes_count = buffer[PAGE_FRAGMENTED_BYTES_COUNT_OFFSET];
+
+    Ok(page::PageHeader {
+        page_type,
+        first_freeblock,
+        cell_count,
+        cell_content_offset,
+        fragmented_bytes_count,
+    })
+}
+
+fn parse_cell_pointers(buffer: &[u8], n: usize, ptr_offset: u16) -> Vec<u16> {
+    let mut pointers = Vec::with_capacity(n);
+    for i in 0..n {
+        pointers.push(read_be_word_at(buffer, 2 * i) - ptr_offset);
+    }
+
+    pointers
+}
+
+fn parse_table_leaf_cell(mut buffer: &[u8]) -> Result<page::TableLeafCell, anyhow::Error> {
+    let (n, size) = read_varint_at(buffer, 0);
+    buffer = &buffer[n as usize..];
+
+    let (n, row_id) = read_varint_at(buffer, 0);
+    buffer = &buffer[n as usize..];
+
+    let payload = buffer[..size as usize].to_vec();
+
+    Ok(page::TableLeafCell {
+        size,
+        row_id,
+        payload,
+    })
+}
+
+/// Read a variable length integer (`varint`) from the specified offset in a buffer.
+///
+/// This function interprets a sequence of bytes starting from the given `offset` in the `buffer`,
+/// decoding a varint using a format where each byte contributes 7 bits to the result. Bytes with
+/// the most significant bit set (`1xxx_xxxx`) indicate that additional bytes follow, while the
+/// last byte in the sequence most significant bit unset (`0xxx_xxxx`).
+///
+/// # Returns
+///
+/// A tuple `(u8, i64)` where:
+/// * The `u8` value represents the number of bytes read from the buffer (i.e. the size of the varint)
+/// * The `i64` value is the decoded integer result.
+///
+/// # Panics
+///
+/// This function will panic if `offset` is out of bounds for the `buffer`.
+///
+/// # Note
+///
+/// The function reads at most 8 bytes, allowing it to decode integers up to 56 bits. For larger
+/// values, it will stop reading after 8 bytes.
+fn read_varint_at(buffer: &[u8], mut offset: usize) -> (u8, i64) {
+    let mut size = 0;
+    let mut result = 0;
+
+    while size < 8 && buffer[offset] >= 0b1000_0000 {
+        result |= ((buffer[offset] as i64) & 0b0111_1111) << (7 * size);
+        offset += 1;
+        size += 1;
+    }
+
+    result |= (buffer[offset] as i64) << (7 * size);
+
+    (size + 1, result)
 }
 
 fn read_be_word_at(input: &[u8], offset: usize) -> u16 {
